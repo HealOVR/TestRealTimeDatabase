e9e822cee14091ea1992efb8900226c0de8034d5
ftnism <fatinishmam@gmail.com>
2021-04-27 07:24:52 +0600

    Fatin's PC

--- Assets/formRegistry.cs
+++ Assets/formRegistry.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.Networking;
 
 public class formRegistry : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class formRegistry : MonoBehaviour
     public InputDeviceCharacteristics controllerCharacteristics;
 
     [SerializeField]
-    private string BaseURL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfO7ExTbUAt3yblEGpZryQZlg53F4IZE8UWz7GTy8jjPWIJlQ/formResponse";
+    private string BaseURL = "https://testproject-d51e9-default-rtdb.firebaseio.com.json";
 
     // Start is called before the first frame update
     void Start()
@@ -26,27 +27,81 @@ public class formRegistry : MonoBehaviour
         }
         else
         {
-            targetDevice.TryGetFeatureValue(CommonUsages.secondary2DAxis, out Vector2 axisValue);
-            targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggered);
-
+            //targetDevice.TryGetFeatureValue(CommonUsages.secondary2DAxis, out Vector2 axisValue);
+            //targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggered);
+            Vector2 axisValue = new Vector2();
+            axisValue.x = 3.7f;
+            axisValue.y = 2.8f;
             StartCoroutine(Post(axisValue.x, axisValue.y));
         }
     }
 
-    IEnumerator Post(float x, float y)
+     IEnumerator Post(float x, float y)
+     {
+         WWWForm form = new WWWForm();
+         form.AddField("x-val", x.ToString());
+         form.AddField("y-val", y.ToString());
+         byte[] rawData = form.data;
+
+         Debug.Log("X = "+x+" Y = "+y);
+
+         WWW www = new WWW(BaseURL, rawData);
+         yield return www;
+     }
+
+    /*public void SendPostRequest(string x, string y)
     {
-        WWWForm form = new WWWForm();
-        form.AddField("entry.1464834321", x.ToString());
-        form.AddField("entry.701621815", y.ToString());
-        byte[] rawData = form.data;
-
-        Debug.Log("X = "+x+" Y = "+y);
-
-        WWW www = new WWW(BaseURL, rawData);
-        yield return www;
+        string Message = x + "  " + y;
+        StartCoroutine(SendPR(Message));
     }
 
-    void TryInitialize()
+    public void SendGetRequest()
+    {
+        string Message = GetInput.text;
+        StartCoroutine(SendGR(Message));
+    }
+
+    IEnumerator SendPR(string Message)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("unitypost", Message);
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/tutorials/unityphppostget/", form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                Debug.Log("Response Text from the server = " + responseText);
+            }
+        }
+    }
+
+    IEnumerator SendGR(string Message)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/tutorials/unityphppostget/?unityget=" + Message))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                Debug.Log("Response Text from the server = " + responseText);
+            }
+        }
+    }*/
+
+
+        void TryInitialize()
     {
         List<InputDevice> devices = new List<InputDevice>();
 
